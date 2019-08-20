b95f0501ac8296341cb45af99f0bf9b8cd9dffcf
AZch <antonzhuikov@gmail.com>
2019-08-20 20:50:52 +0700

    Feature: add find functions with DB

---
+++ dbData/findQuestions.js
@@ -0,0 +1,17 @@
+var User = require('../shemas/user');
+
+async function getUserByName(name) {
+    return await User.findOne({ name: name }).populate('games');
+}
+
+async function findGameById(id) {
+    return await Game.
+    findOne({ _id: new ObjectID(id) }).
+    populate('steps');
+}
+
+async function getUserById(id) {
+    return await User.findOne({ _id: id }).populate('games');
+}
+
+export { getUserByName, findGameById, getUserById };
